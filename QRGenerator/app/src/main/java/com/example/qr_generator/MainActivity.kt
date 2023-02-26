49ee3700cda99d9a8cd88b10a508ca426d7f2c2b
trypuzz <iv_tripuz@mail.ru>
2023-02-26 17:37:09 +0300

    Encode QR in Text (Generation + Scanner w/o links)

--- QRGenerator/app/src/main/java/com/example/qr_generator/MainActivity.kt
+++ QRGenerator/app/src/main/java/com/example/qr_generator/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.qr_generator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidmads.library.qrgenearator.QRGContents
+import androidmads.library.qrgenearator.QRGEncoder
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+    var im: ImageView? = null
+    var bGenerate: Button? = null
+    var bScanQR: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bGenerate = findViewById(R.id.button)
+        im = findViewById(R.id.imageView)
+        bScanQR = findViewById(R.id.bScan)
+        bGenerate?.setOnClickListener {
+            generateQRCode("TEST TEXT")
+        }
+        bScanQR?.setOnClickListener{
+            startActivity(Intent(this, ScanQR::class.java))
+        }
+    }
+
+    private fun generateQRCode(text: String) {
+        val qrGenerator = QRGEncoder(text, null, QRGContents.Type.TEXT, 700)
+        try {
+            val bMap = qrGenerator.bitmap
+            im?.setImageBitmap(bMap)
+        } catch (e: java.lang.Exception) {
+
+        }
     }
 }
---
+++ QRGenerator/app/src/main/java/com/example/qr_generator/ScanQR.kt
@@ -0,0 +1,76 @@
+package com.example.qr_generator
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+import com.google.zxing.Result
+import me.dm7.barcodescanner.zbar.ZBarScannerView
+import me.dm7.barcodescanner.zxing.ZXingScannerView
+
+class ScanQR : AppCompatActivity(), ZBarScannerView.ResultHandler {
+    private lateinit var codeScanner: CodeScanner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_qr)
+        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+
+        codeScanner = CodeScanner(this, scannerView)
+
+        // Parameters (default values)
+        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
+        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
+        // ex. listOf(BarcodeFormat.QR_CODE)
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+        codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
+        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
+        codeScanner.isFlashEnabled = false // Whether to enable flash or not
+
+        // Callbacks
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
+                var clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                var clip = ClipData.newPlainText("label", it.text)
+                clipboard.setPrimaryClip(clip)
+            }
+            finish()
+        }
+        codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
+            runOnUiThread {
+                Toast.makeText(
+                    this, "Camera initialization error: ${it.message}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    override fun handleResult(p0: me.dm7.barcodescanner.zbar.Result?) {
+        TODO("Not yet implemented")
+    }
+}
