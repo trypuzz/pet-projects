b83cb1eda21fccbfc9dcd15082a1d94492d598fe
trypuzz <iv_tripuz@mail.ru>
2023-02-26 22:04:36 +0300

    Initial RSS

---
+++ RSSreader/app/src/main/java/com/example/rss_reader/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rss_reader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
