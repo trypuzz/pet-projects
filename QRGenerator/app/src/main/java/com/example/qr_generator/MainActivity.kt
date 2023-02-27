56abd7686b30f0d7f3356d4982d654968f5e7239
trypuzz <iv_tripuz@mail.ru>
2023-02-27 16:24:39 +0300

    Add Plain Text to enter text on Main Activity

--- QRGenerator/app/src/main/java/com/example/qr_generator/MainActivity.kt
+++ QRGenerator/app/src/main/java/com/example/qr_generator/MainActivity.kt
@@ -1,34 +1,40 @@
 package com.example.qr_generator
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidmads.library.qrgenearator.QRGContents
 import androidmads.library.qrgenearator.QRGEncoder
 import androidx.activity.result.contract.ActivityResultContracts
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     var im: ImageView? = null
     var bGenerate: Button? = null
     var bScanQR: Button? = null
-
+    var editText : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bGenerate = findViewById(R.id.button)
         im = findViewById(R.id.imageView)
         bScanQR = findViewById(R.id.bScan)
+        editText = findViewById(R.id.editText)
         bGenerate?.setOnClickListener {
-            generateQRCode("https://ru.stackoverflow.com/questions/965315/%D0%9A%D0%B0%D0%BA-%D0%BF%D1%80%D0%B8-%D0%BE%D1%82%D0%BA%D1%80%D1%8B%D1%82%D0%B8%D0%B8-%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F-%D0%BE%D1%82%D0%BA%D1%80%D1%8B%D0%B2%D0%B0%D1%82%D1%8C-%D1%81%D1%81%D1%8B%D0%BB%D0%BA%D1%83-%D0%B2-%D0%B1%D1%80%D0%B0%D1%83%D0%B7%D0%B5%D1%80%D0%B5")
+            generateQRCode(editText?.text.toString())
         }
         bScanQR?.setOnClickListener{
             startActivity(Intent(this, ScanQR::class.java))
         }
+
     }
 
     private fun generateQRCode(text: String) {
+
         val qrGenerator = QRGEncoder(text, null, QRGContents.Type.TEXT, 700)
         try {
             val bMap = qrGenerator.bitmap
