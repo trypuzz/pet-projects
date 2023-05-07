6fa57093b161496fac5c27cf937b4355a39acefa
trypuzz <iv_tripuz@mail.ru>
2023-05-07 14:39:38 +0300

    Unversioned Files

---
+++ QRGenerator/app/build/generated/ap_generated_sources/debug/out/com/example/qr_generator/BR.java
@@ -0,0 +1,5 @@
+package com.example.qr_generator;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ QRGenerator/app/build/generated/data_binding_trigger/debug/com/example/qr_generator/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.qr_generator;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
