088cd66c2bba97ea1cfd62d35b2d70c4837a6d09
trypuzz <iv_tripuz@mail.ru>
2023-06-08 11:03:12 +0300

    Deleted projects

--- Disney-Heroes/app/src/main/java/com/example/disneyheroes/network/models/AllDisneyHeroesResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.example.disneyheroes.network.models
-
-data class AllDisneyHeroesResponse(
-    val count: Int,
-    val data: List<DisneyHeroResponse>
-)
--- PokeAPIList/app/build/generated/data_binding_trigger/debug/com/example/pokeapi_list/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.pokeapi_list;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
